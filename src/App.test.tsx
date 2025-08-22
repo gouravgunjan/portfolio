1fbb4ec13cf708120e90ca07f88ddeb86b2290e7
gouravgunjan <gourav.gunjan@gmail.com>
2025-08-22 11:56:13 +0530

    feat: add testing infrastructure for unit, a11y, and e2e tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+expect.extend(toHaveNoViolations);
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('should have no accessibility violations', async () => {
+  const { container } = render(<App />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
 });
